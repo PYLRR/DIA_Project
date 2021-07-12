0cc17be1edf7034de0925d30b7964620105e70d4
Le Rolland-Raumer Pierre-Yves <pierre-yves.le-rolland-raumer@insa-rennes.fr>
2021-07-12 12:53:50 +0200

    adding regret plot to Q4

--- Q3/Q3.py
+++ Q3/Q3.py
@@ -17,54 +17,63 @@ AdQualitiesVector = np.clip(np.random.normal(0.5, 0.1, auctionHouse.NB_ADVERTISE
 ### GRAPH
 graph = graph.Graph()
 
-### BIDS/AUCTIONS
 # randomize the bids, may be improved later
 bids = np.random.randint(0, auctionHouse.MAX_BID + 1,
                          (auctionHouse.NB_ADVERTISERS, auctionHouse.NB_CATEGORIES))
-bids[0] = np.zeros(auctionHouse.NB_CATEGORIES)  # set bids of current advertiser to 0
 
-previousReward = 0  # when bids are 0, the reward will be 0
-currentImprovedCategory = 0  # to jump to 0 at next iteration
-nbOfTurnsWithoutImprovement = 0  # if it reaches NB_CATEGORIES, the greedy algorithm stops
-stabilized = False
-while not stabilized:
-    bids[0, currentImprovedCategory] += 1
 
-    # if this bid reached the maximum, it is considered as a no-improvement
-    if bids[0, currentImprovedCategory] > auctionHouse.MAX_BID:
-        bids[0, currentImprovedCategory] -= 1
-        nbOfTurnsWithoutImprovement += 1
-        if nbOfTurnsWithoutImprovement >= auctionHouse.NB_CATEGORIES:
-            break
+def greedy(graph, bids):
+    ### BIDS/AUCTIONS
+    # sets bids of learning advertiser to 0
+    bids[0] = np.zeros(auctionHouse.NB_CATEGORIES)  # set bids of current advertiser to 0
+
+    previousReward = 0  # when bids are 0, the reward will be 0
+    currentImprovedCategory = 0  # to jump to 0 at next iteration
+    nbOfTurnsWithoutImprovement = 0  # if it reaches NB_CATEGORIES, the greedy algorithm stops
+    stabilized = False
+    while not stabilized:
+        bids[0, currentImprovedCategory] += 1
+
+        # if this bid reached the maximum, it is considered as a no-improvement
+        if bids[0, currentImprovedCategory] > auctionHouse.MAX_BID:
+            bids[0, currentImprovedCategory] -= 1
+            nbOfTurnsWithoutImprovement += 1
+            if nbOfTurnsWithoutImprovement >= auctionHouse.NB_CATEGORIES:
+                break
+            currentImprovedCategory = (currentImprovedCategory + 1) % auctionHouse.NB_CATEGORIES
+            continue
+
+        # gets winner of each category
+        winners = auctionHouse.runAuction(bids)
+        learningAdvertiserWonAuctions = graph.updateFromAuctionResult(winners, AD_QUALITY)
+
+        ### MONTECARLO
+        activationProbabilities = MonteCarlo.run(graph, graph.seeds, 2000)[1]
+
+        # compute gain
+        gain = 0
+        for i in range(graph.nbNodes):
+            cat = graph.categoriesPerNode[i]
+            slot = learningAdvertiserWonAuctions[cat]
+            profit = VALUE_OF_CLICK
+            cost = auctionHouse.computeVCG(0, bids, AdQualitiesVector, cat, slot)
+            gain += activationProbabilities[i] * (profit - cost)
+
+        if gain >= previousReward:  # we improved
+            previousReward = gain
+            nbOfTurnsWithoutImprovement = 0
+        else:  # we didnt't improve : reset last modification
+            bids[0, currentImprovedCategory] -= 1
+            nbOfTurnsWithoutImprovement += 1
+            if nbOfTurnsWithoutImprovement >= auctionHouse.NB_CATEGORIES:
+                break
+
+        # change category from which we improve bid
         currentImprovedCategory = (currentImprovedCategory + 1) % auctionHouse.NB_CATEGORIES
-        continue
 
-    # gets winner of each category
-    winners = auctionHouse.runAuction(bids)
-    learningAdvertiserWonAuctions = graph.updateFromAuctionResult(winners, AD_QUALITY)
+    return bids, previousReward
 
-    ### MONTECARLO
-    activationProbabilities = MonteCarlo.run(graph, graph.seeds, 2000)[1]
 
-    # compute gain
-    gain = 0
-    for i in range(graph.nbNodes):
-        cat = graph.categoriesPerNode[i]
-        slot = learningAdvertiserWonAuctions[cat]
-        profit = VALUE_OF_CLICK
-        cost = auctionHouse.computeVCG(0, bids, AdQualitiesVector, cat, slot)
-        gain += activationProbabilities[i] * (profit - cost)
-
-    if gain >= previousReward:  # we improved
-        previousReward = gain
-        nbOfTurnsWithoutImprovement = 0
-    else:  # we didnt't improve : reset last modification
-        bids[0, currentImprovedCategory] -= 1
-        nbOfTurnsWithoutImprovement += 1
-        if nbOfTurnsWithoutImprovement >= auctionHouse.NB_CATEGORIES:
-            break
-
-    # change category from which we improve bid
-    currentImprovedCategory = (currentImprovedCategory + 1) % auctionHouse.NB_CATEGORIES
-
-print(bids)
+bestBids, bestReward = greedy(graph, bids)
+print(bestBids)
+print(bestReward)
--- Q4/Q4.py
+++ Q4/Q4.py
@@ -2,11 +2,12 @@ import numpy as np
 from LinearMabEnvironment import LinearMabEnvironment
 from LinUcbLearner import LinUcbLearner
 import environment.auctionHouse as auctionHouse
+import matplotlib.pyplot as plt
 
 np.random.seed(1)
 
-n_arms = 20
-n_experiments = 500
+n_arms = 15
+n_experiments = 1000
 lin_ucb_rewards_per_experiment = []
 
 env = LinearMabEnvironment(n_arms=n_arms)
@@ -18,7 +19,8 @@ for i in range(n_experiments):
     learner.update(arm, reward)
 
 bestArm = np.argmax(learner.meanRewardPerArm)
-print("best arm : " + str(bestArm) + " with a mean reward of " + str(learner.meanRewardPerArm[bestArm]))
+bestReward = learner.meanRewardPerArm[bestArm]
+print("best arm : " + str(bestArm) + " with a mean reward of " + str(bestReward))
 print("bids of the best arm : " + str(learner.arms[bestArm]))
 
 
@@ -47,7 +49,7 @@ def estimate_ad_quality(dataset, graph, learningAdvertiserWonAuctionsHistory, co
         for i in range(0, len(idx_w_active), 2):
             slot = learningAdvertiserWonAuctions[graph.categoriesPerNode[idx_w_active[i + 1]]]
             if slot == -1:
-                slot = auctionHouse.NB_SLOTS_PER_CATEGORY-1 # last slot if no one allocated
+                slot = auctionHouse.NB_SLOTS_PER_CATEGORY - 1  # last slot if no one allocated
             # we don't need to browse for previously activated nodes.
             # indeed, a node only activates if its fictitious node was previously activated. So we know it has been.
             # it will be an addition of 1 as we have only 1 ancestor
@@ -58,14 +60,14 @@ def estimate_ad_quality(dataset, graph, learningAdvertiserWonAuctionsHistory, co
             -1)  # find fictitious nodes, excluding active ones
 
         for i in range(0, len(idx_v_active), 2):
-            slot = learningAdvertiserWonAuctions[graph.categoriesPerNode[idx_v_active[i + 1]-graph.nbNodes]]
+            slot = learningAdvertiserWonAuctions[graph.categoriesPerNode[idx_v_active[i + 1] - graph.nbNodes]]
             if slot == -1:
                 slot = auctionHouse.NB_SLOTS_PER_CATEGORY - 1  # last slot if no one allocated
             fictitiousActivationsForGivenSlot[slot] += 1
 
         episodeCount += 1
 
-    maxSlotToConsider = auctionHouse.NB_SLOTS_PER_CATEGORY-1
+    maxSlotToConsider = auctionHouse.NB_SLOTS_PER_CATEGORY - 1
     if considerOnlyFirstSlot:
         maxSlotToConsider = 0
 
@@ -77,22 +79,59 @@ def estimate_ad_quality(dataset, graph, learningAdvertiserWonAuctionsHistory, co
         else:
             clickProbabilityForCurrentSlot = activationsForGivenSlot[slot] / fictitiousActivationsForGivenSlot[slot]
             divider += fictitiousActivationsForGivenSlot[slot]
-        totalEstimation += fictitiousActivationsForGivenSlot[slot] * clickProbabilityForCurrentSlot / auctionHouse.SLOT_PROMINENCES[slot]
+        totalEstimation += fictitiousActivationsForGivenSlot[slot] * clickProbabilityForCurrentSlot / \
+                           auctionHouse.SLOT_PROMINENCES[slot]
     totalEstimation /= divider
 
     return totalEstimation, divider
+
 
 print("-------------")
 
 estimation, n = estimate_ad_quality(env.history, env.graph, env.learningAdvertiserWonAuctionsHistory)
 
-print("estimation of ad quality with first slot considered : " + str(estimation) + " vs real : " + str(env.adQualitiesVector[0]))
-d = 1.96*(estimation*(1-estimation))**0.5/n**0.5
-print("Confidence interval with 95% : ["+str(estimation-d)+";"+str(estimation+d)+"]")
+print("estimation of ad quality with first slot considered : " + str(estimation) + " vs real : " + str(
+    env.adQualitiesVector[0]))
+d = 1.96 * (estimation * (1 - estimation)) ** 0.5 / n ** 0.5
+print("Confidence interval with 95% : [" + str(estimation - d) + ";" + str(estimation + d) + "]")
 
 print("-------------")
 
 estimation, n = estimate_ad_quality(env.history, env.graph, env.learningAdvertiserWonAuctionsHistory, False)
-print("estimation of ad quality with all slots considered : " + str(estimation) + " vs real : " + str(env.adQualitiesVector[0]))
-d = 1.96*(estimation*(1-estimation))**0.5/n**0.5
-print("Confidence interval with 95% : ["+str(estimation-d)+";"+str(estimation+d)+"]")
+print("estimation of ad quality with all slots considered : " + str(estimation) + " vs real : " + str(
+    env.adQualitiesVector[0]))
+d = 1.96 * (estimation * (1 - estimation)) ** 0.5 / n ** 0.5
+print("Confidence interval with 95% : [" + str(estimation - d) + ";" + str(estimation + d) + "]")
+
+
+meanReward = [learner.collected_rewards[0]]
+for i in range(1, n_experiments):
+    reward = learner.collected_rewards[i]
+    meanReward.append(i * meanReward[i - 1] / (i + 1) + reward / (i + 1))
+
+x = range(n_experiments)
+ymax = np.full(n_experiments, bestReward)
+
+plt.xlabel('number of iterations')
+plt.ylabel('mean reward')
+plt.title('Mean reward in function of iterations')
+plt.plot(x, meanReward, 'k-')
+plt.plot(x,ymax, 'r-')
+plt.show()
+
+y = np.cumsum(learner.collected_rewards)
+ymaxCum = np.cumsum(ymax)
+plt.xlabel('number of iterations')
+plt.ylabel('cumulated reward')
+plt.title('Cumulated reward in function of iterations')
+plt.plot(x, y, 'k-')
+plt.plot(x,ymaxCum, 'r-')
+plt.show()
+
+y = np.cumsum(learner.collected_rewards)
+ymaxCum = np.cumsum(ymax)
+plt.xlabel('number of iterations')
+plt.ylabel('estimated regret')
+plt.title('Estimated regret in function of iterations')
+plt.plot(x, ymaxCum-y, 'k-')
+plt.show()
